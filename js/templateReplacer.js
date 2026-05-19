e8ec6fc296a8981d8a3f75085afe85d7fc3f6186
DiegoURJC1 <d.gil.2021@alumnos.urjc.es>
2026-05-19 19:47:49 +0200

    Fix templateReplacer.js bug
    This bug appears when an element has an id but no template associated

--- js/templateReplacer.js
+++ js/templateReplacer.js
@@ -1,12 +1,12 @@
 /**
- * This funcion places all templates with a certain ``id``
+ * This function places all templates with a certain ``id``
  * into ``div``s with the same ``class`` name
  */
 function replateTemplates() {
     document.querySelectorAll('div').forEach(div => {
         div.classList.forEach(cls => {
             const template = document.getElementById(cls);
-            if (template) {
+            if (template && template.content) {
                 div.appendChild(template.content.cloneNode(true));
             }
         });
